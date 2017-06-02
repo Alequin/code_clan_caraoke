b5799635dc3f846f367d9939ff5d33c3c5ec1ad2
James Cox <j-cox03@hotmail.com>
2017-06-02 13:32:39 +0100

    Made and passed basic tests for the Guest class

---
+++ guest.rb
@@ -0,0 +1,35 @@
+class Guest
+
+  attr_reader :name, :money, :favourite_song
+
+  def initialize(name, money, favourite_song)
+    @name = name
+    @money = money
+    @favourite_song = favourite_song
+  end
+
+  def increase_money_by(amount)
+    @money += amount
+  end
+
+  def decrease_money_by(amount)
+    @money -= amount
+  end
+
+  def cheer()
+    return "Whoo!"
+  end
+
+  def sing(song)
+    return song.lyrics
+  end
+
+  def sing_favourite_song()
+    return @favourite_song.lyrics
+  end
+
+  def is_favourite_song?(song)
+    return @favourite_song == song
+  end
+
+end
---
+++ specs/guest_spec.rb
@@ -0,0 +1,56 @@
+require("minitest/autorun")
+require("minitest/rg")
+require_relative("../song.rb")
+require_relative("../guest.rb")
+
+class TestGuest < Minitest::Test
+
+  def setup
+    @song_1_lyrics = "My daddy was workin' nine to five"
+    @song_2_lyrics = "Somebody once told me the world is gonna roll me"
+    @song_1 = Song.new("Rock’n’Roll Singer", "ACDC", @song_1_lyrics, 228)
+    @song_2 = Song.new("All Star", "Smash Mouth", @song_2_lyrics, 237)
+    @guest_1 = Guest.new("Bob", 100, @song_1)
+  end
+
+  def test_get_name
+    assert_equal("Bob", @guest_1.name)
+  end
+
+  def test_get_money_amount
+    assert_equal(100, @guest_1.money)
+  end
+
+  def test_get_fav_song
+    assert_equal(@song_1, @guest_1.favourite_song)
+  end
+
+  def test_increase_money
+    @guest_1.increase_money_by(100)
+    assert_equal(200, @guest_1.money)
+  end
+
+  def test_decrease_money
+    @guest_1.decrease_money_by(50)
+    assert_equal(50, @guest_1.money)
+  end
+
+  def test_cheer
+    assert_equal("Whoo!", @guest_1.cheer())
+  end
+
+  def test_sing
+    assert_equal(@song_2.lyrics, @guest_1.sing(@song_2))
+  end
+
+  def test_sing_favourite
+    expected = @guest_1.favourite_song.lyrics
+    assert_equal(expected, @guest_1.sing_favourite_song())
+  end
+
+  def test_is_favourite_song
+    assert_equal(true, @guest_1.is_favourite_song?(@song_1))
+    assert_equal(false, @guest_1.is_favourite_song?(@song_2))
+  end
+
+end
