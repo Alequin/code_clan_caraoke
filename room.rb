7e3fcadfbefdcd0422ff2653e69f86b4c3aff7ed
James Cox <j-cox03@hotmail.com>
2017-06-02 13:48:39 +0100

    Made changes in class Room to match the class diagram

--- room.rb
+++ room.rb
@@ -1,12 +1,15 @@
 class Room
 
+  @@total_rooms = 0
+
   def initialize(guests, songs, capacity, entry_fee)
     @guests = guests
     @songs = songs
     @capacity = capacity
     @entry_fee = entry_fee
 
-    @@door_number += 1
+    @@total_rooms += 1
+    @room_number = @@total_rooms
     @current_song_index = 0
   end
 
