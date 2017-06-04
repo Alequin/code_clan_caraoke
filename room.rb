222dc556efd7732312be1c67f4dd1773375e47ac
James Cox <j-cox03@hotmail.com>
2017-06-04 21:35:18 +0100

    Made get_current_song method pass tests, altered the run order of play_next_song to work better with the requirements of get_current_song

--- room.rb
+++ room.rb
@@ -14,17 +14,22 @@ class Room
 
     @@total_rooms += 1
     @room_number = @@total_rooms
-    @current_song_index = 0
+    @current_song_index = -1
   end
 
   def Room.reset_total_rooms_counter()
     @@total_rooms = 0
   end
 
+  def get_current_song()
+    return nil if (@current_song_index < 0)
+    return @songs[@current_song_index]
+  end
+
   def play_next_song()
+    @current_song_index += 1 if @current_song_index < @songs.length
     @current_song_index = 0 if @current_song_index >= @songs.length
     lyrics = @songs[@current_song_index].lyrics
-    @current_song_index += 1 if @current_song_index < @songs.length
     return lyrics
   end
 
