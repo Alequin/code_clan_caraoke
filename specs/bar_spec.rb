b8746ca9eda57b70dc76e1b15349e16a38e87242
James Cox <j-cox03@hotmail.com>
2017-06-04 22:47:23 +0100

    Added bodies of tests for Bar class

--- specs/bar_spec.rb
+++ specs/bar_spec.rb
@@ -1,7 +1,41 @@
 require("minitest/autorun")
 require("minitest/rg")
 require_relative("../bar.rb")
+require_relative("../guest.rb")
 
 class TestBar < Minitest::Test
+
+  def setup
+    @guest_1 = Guest.new("Bob", 100, @song_1)
+    @bar = Bar.new
+  end
+
+  def test_get_profit
+
+  end
+
+  def test_get_menu
+
+  end
+
+  def test_take_money
+
+  end
+
+  def test_sell_to_guest
+
+  end
+
+  def test_add_consumable
+
+  end
+
+  def test_remove_consumable
+
+  end
+
+  def test_consumable_available
+
+  end
 
 end
